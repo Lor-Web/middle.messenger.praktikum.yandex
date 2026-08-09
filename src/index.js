5d8cd85c1e98bc1ff154b52d49c361fa8dc8eed8
Ruslan Ruya <ruslan.ruya@rt.ru>
2026-08-09 17:10:13 +0300

    sprint_1: Страница Регистрации и компонент карточки

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Handlebars from "handlebars";
 
 import authTpl from "./pages/auth.hbs?raw";
+import registerTpl from "./pages/register.hbs?raw";
 
 import buttonTpl from "./components/button/button.hbs?raw";
 import inputTpl from "./components/input/input.hbs?raw";
+import cardTpl from "./components/card/card.hbs?raw";
 
 Handlebars.registerPartial("button", buttonTpl);
 Handlebars.registerPartial("input", inputTpl);
+Handlebars.registerPartial("card", cardTpl);
 
 document.body.innerHTML = Handlebars.compile(authTpl)();
