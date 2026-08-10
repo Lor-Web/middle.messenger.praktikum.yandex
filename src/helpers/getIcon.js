03ac52338fe9df526a0c89881e29383f4320cc19
Ruslan Ruya <ruslan.ruya@rt.ru>
2026-08-10 16:33:08 +0300

    sprint_1: Футер окна чата

--- src/helpers/getIcon.js
+++ src/helpers/getIcon.js
@@ -1,7 +1,7 @@
 import arrowRight from "../../public/icons/arrow-right-solid-full.svg?raw";
 import ellipsisVertical from "../../public/icons/ellipsis-vertical.svg?raw";
 import magnifyingGlass from "../../public/icons/magnifying-glass-solid-full.svg?raw";
-import paperclip from "../../public/icons/paperclip-solid-full.svg?raw";
+import paperclip from "../../public/icons/paperclip.svg?raw";
 import checkDouble from "../../public/icons/check-double.svg?raw";
 
 const ICONS = {
