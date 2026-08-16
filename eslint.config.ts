868063b808a0a53bc1ada3f93b0d3514c641619a
Ruslan Ruya <ruslan.ruya@rt.ru>
2026-08-16 16:55:32 +0300

    sprint_2: Правила для eslint && prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(["dist/**/*"]),
+  globalIgnores(['dist/**/*']),
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js, 'simple-import-sort': simpleImportSort },
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
     rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
-      "sort-vars": "error",
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
     },
   },
   tseslint.configs.recommended,
+  prettier,
 ]);
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard-scss"],
+  extends: ["stylelint-config-standard-scss", "stylelint-config-prettier-scss"],
   ignoreFiles: ["./src/styles/base/_normalized.scss"],
   rules: {
     // Разрешаем пустые строки между группами Sass-переменных
