c3b9de0fd496523ccc74c3054d17ead5dd2ba5fd
Ruslan Ruya <ruslan.ruya@rt.ru>
2026-08-16 16:56:19 +0300

    sprint_2: lint fix

--- src/helpers/getIcon.ts
+++ src/helpers/getIcon.ts
@@ -1,9 +1,9 @@
 import arrowRight from "../../public/icons/arrow-right-solid-full.svg?raw";
-import ellipsisVertical from "../../public/icons/ellipsis-vertical.svg?raw";
-import paperclip from "../../public/icons/paperclip.svg?raw";
 import checkDouble from "../../public/icons/check-double.svg?raw";
+import ellipsisVertical from "../../public/icons/ellipsis-vertical.svg?raw";
 import gear from "../../public/icons/gear.svg?raw";
 import logout from "../../public/icons/logout.svg?raw";
+import paperclip from "../../public/icons/paperclip.svg?raw";
 
 const ICONS = {
   "arrow-right": arrowRight,
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from "vite";
-
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
 
 // используйте его. До этой строчки process.env не будет содержать указанных в .env переменных. После — будет
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./src",
-  publicDir: "../public",
+  root: './src',
+  publicDir: '../public',
   server: {
     open: true,
     port: process.env.PORT || 3000,
@@ -17,12 +16,12 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         // Uses the faster, modern Sass compiler API
-        api: "modern-compiler",
+        api: 'modern-compiler',
       },
     },
   },
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     emptyOutDir: true,
   },
 });
